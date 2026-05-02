987aa9e60b6a7a624980605c9989ae75a4763d80
Alexandre Mutel <alexandre_mutel@live.com>
2026-05-02 18:04:22 +0200

    Add plugin abstractions project

---
+++ src/CodeAlta.Plugins.Abstractions.Tests/PlaceholderTests.cs
@@ -0,0 +1,11 @@
+namespace CodeAlta.Plugins.Abstractions.Tests;
+
+[TestClass]
+public sealed class PlaceholderTests
+{
+    [TestMethod]
+    public void ProjectLoads()
+    {
+        Assert.IsTrue(true);
+    }
+}
